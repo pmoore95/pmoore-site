7d1f086ef1a7df7389f33aee9de5fb1b8887693f
pmoore95 <57687000+pmoore95@users.noreply.github.com>
2020-07-28 19:02:32 +0100

    Updates

---
+++ precache-manifest.0c0987487dd8b899d7dba2f29e08e5ec.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d865a1d980d0182d54152703c92306e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f61cf3e31d177dece56",
+    "url": "/static/css/main.9f40a66f.chunk.css"
+  },
+  {
+    "revision": "6f3fb921b88cb49b0089",
+    "url": "/static/js/2.10a2e58c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10a2e58c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f61cf3e31d177dece56",
+    "url": "/static/js/main.e2907226.chunk.js"
+  },
+  {
+    "revision": "30d2442e8235325d3d4e",
+    "url": "/static/js/runtime-main.dd28403e.js"
+  },
+  {
+    "revision": "4d0c761fe97885e97e379b41c506c689",
+    "url": "/static/media/b2.4d0c761f.jpg"
+  },
+  {
+    "revision": "cfecb2da14ecb652a50758fd12f2d5cb",
+    "url": "/static/media/bootstrap.cfecb2da.png"
+  },
+  {
+    "revision": "e7b8fcd4ceb430ffc60ed55e5ab98fa5",
+    "url": "/static/media/cs.e7b8fcd4.png"
+  },
+  {
+    "revision": "4a07cc44568feafad23407c5a118aad0",
+    "url": "/static/media/css3.4a07cc44.png"
+  },
+  {
+    "revision": "24a5a6176a51c29b8cda9725c73100f1",
+    "url": "/static/media/git.24a5a617.png"
+  },
+  {
+    "revision": "664a8c00ef526bf33edf55808601d461",
+    "url": "/static/media/html5.664a8c00.png"
+  },
+  {
+    "revision": "bacc7f58f9748a148122e84a2dd32856",
+    "url": "/static/media/hygienehub-mobile.bacc7f58.png"
+  },
+  {
+    "revision": "a9d024c5474ac32a8fceb6a42055f87e",
+    "url": "/static/media/hygienehub.a9d024c5.png"
+  },
+  {
+    "revision": "d5c01b5b488c32eaf5eb07725e7958a0",
+    "url": "/static/media/java.d5c01b5b.png"
+  },
+  {
+    "revision": "30ddcf786e53928f6330291b84501038",
+    "url": "/static/media/javascript.30ddcf78.png"
+  },
+  {
+    "revision": "da858aabbb78c8b0edbb396cc32cce9d",
+    "url": "/static/media/jquery.da858aab.png"
+  },
+  {
+    "revision": "3a727dd2067e78d436b19c841acec177",
+    "url": "/static/media/mysql.3a727dd2.png"
+  },
+  {
+    "revision": "426d404b72c7ab911449d669b8ab0dc0",
+    "url": "/static/media/php.426d404b.png"
+  },
+  {
+    "revision": "b7c8451c7125f1fb666b3b7bb22547cc",
+    "url": "/static/media/pm_2020.b7c8451c.pdf"
+  },
+  {
+    "revision": "e8869205668e7c735fa82c89f27e908a",
+    "url": "/static/media/pmoore.e8869205.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/static/media/react.37fe8322.png"
+  },
+  {
+    "revision": "c50dc91cbc73fcebfade7e07092ff307",
+    "url": "/static/media/rymer1.c50dc91c.png"
+  },
+  {
+    "revision": "97bab104216260bf0fb89fe9007c7b53",
+    "url": "/static/media/rymer2.97bab104.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pmoore-site/precache-manifest.bd9c978a2b33ff76dac8f468943b3062.js"
+  "/precache-manifest.0c0987487dd8b899d7dba2f29e08e5ec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pmoore-site/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
