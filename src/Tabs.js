3dfce38982719d5c5b786a4cb9924d47a0617df1
pmoore95 <57687000+pmoore95@users.noreply.github.com>
2020-08-07 21:09:58 +0100

    1

--- src/Tabs.js
+++ src/Tabs.js
@@ -63,18 +63,17 @@ class Tabs extends React.Component {
                 <div className={this.assignClass('Skills')}>
                     <h1>Skills</h1>
                     <div id="skills-container">
+                        <Skill title='Php' image={php} experience="2 years"/>
+                        <Skill title='Javascript' image={js} experience="4 years"/>
+                        <Skill title='React' image={react} experience="1 year"/>
+                        <Skill title='jQuery' image={jq} experience="4 years"/>
+                        <Skill title='Java' image={java} experience="2 years"/>
+                        <Skill title='C#' image={cs} experience="1 year"/>
+                        <Skill title='MySql' image={mysql} experience="3 years"/>
                         <Skill title='HTML 5' image={html5} experience="4 years"/>
                         <Skill title='CSS3' image={css3} experience="4 years"/>
-                        <Skill title='Javascript' image={js} experience="4 years"/>
-                        <Skill title='jQuery' image={jq} experience="4 years"/>
-                        <Skill title='Php' image={php} experience="2 years"/>
-                        <Skill title='MySql' image={mysql} experience="3 years"/>
                         <Skill title='Bootstrap' image={bootstrap} experience="3 years"/>
                         <Skill title='Git' image={git} experience="1 year"/>
-                        <Skill title='React' image={react} experience="< 1 year"/>
-                        <h1 id="other-skills">Other Skills</h1>
-                        <Skill title='Java' image={java} experience="2 years"/>
-                        <Skill title='C#' image={cs} experience="2 years"/>
                     </div>
                 </div>
                 <div id="projects"className={this.assignClass('Projects')}>
